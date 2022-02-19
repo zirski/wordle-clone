498a15fb83653f14f3b5be8e4ff13c208b87b5c9
zirski <tobiasjhammond@gmail.com>
2022-02-18 17:57:49 -0800

    introduced cell matrix

--- main.lua
+++ main.lua
@@ -3,7 +3,8 @@ function love.load()
   cellW = 68.75
   windowBuffer = cellW / 2
   cellH = rectW
-  cellX = 275 - cellW / 2
+  cellX = windowBuffer - cellW / 2
+  cells = 5
   cellColor = {}
     cellColor.r = 105 / 255
     cellColor.g = 105 / 255
@@ -12,16 +13,37 @@ function love.load()
   isPushed = false
 end
 
-function love.draw()
-  love.graphics.setBackgroundColor(0, 0, 0, 1)
-  love.graphics.rectangle("fill", cellX, 100, 68.75, 68.75)
-  love.graphics.setColor(cellColor.r, cellColor.g, cellColor.b, 1)
+-- function love.draw()
+--   love.graphics.setBackgroundColor(0, 0, 0, 1)
+--   for i = 1, cells, 1 do
+--     love.graphics.rectangle("fill", cellX, 100, 68.75, 68.75)
+--     love.graphics.setColor(cellColor.r, cellColor.g, cellColor.b, 1)
+--     cellX = cellX + (cellW + windowBuffer)
+--   end
+-- end
+
+
+
+function generateCells() -- draws cells (pulled straight from Splodey-master)
+  num_cells = 20
+
+  cells = {}
+
+
+
+  for i = 1, num_cells, 1 do
+    local cell = {}
+    cell.x = 20
+    cell.y = 50
+
+    cell.x = cell.x + 10
+    table.insert(cell, cells)
+  end
+
+  print(#cells)
 end
 
-function love.update(dt)
-  if isPushed == true then
-    
-end
+
 
 function love.keypressed(key)
   if key == "escape" then
@@ -30,4 +52,6 @@ function love.keypressed(key)
 
   if key == "a" then
     cellState = true
+    generateCells()
+  end
 end
