d4342ad420d5b15fedcf0b6348e004e35d4517b5
zirski <tobiasjhammond@gmail.com>
2022-03-18 22:22:08 -0700

    pretty much entire new data management - starting work on changing cell states

--- main.lua
+++ main.lua
@@ -1,33 +1,51 @@
 function love.load()
-  word = "audio"
-  cellW = 68.75
-  windowBuffer = cellW / 2
-  cellH = rectW
-  cellX = 275 - cellW / 2
-  cellColor = {}
-    cellColor.r = 105 / 255
-    cellColor.g = 105 / 255
-    cellColor.b = 105 / 255
+  currentCell = 1
 
-  isPushed = false
-end
+  cells = {}
 
-function love.draw()
-  love.graphics.setBackgroundColor(0, 0, 0, 1)
-  love.graphics.rectangle("fill", cellX, 100, 68.75, 68.75)
-  love.graphics.setColor(cellColor.r, cellColor.g, cellColor.b, 1)
+  for i = 1, 5 do
+    cells[i] = {}
+    cells[i].state = false
+    cells[i].x = (70 * i) + 25
+    cells[i].y = 100
+    cells[i].w = 50
+    cells[i].h = cells[i].w
+  end
+
+  for i, v in ipairs(cells) do
+    print(cells[i].x)
+  end
+
+  isPressed = "false"
 end
 
 function love.update(dt)
-  if isPushed == true then
-    
+  if cells[1].state == true then
+    --isPressed = "true"
+  end
 end
+
+function love.draw()
+
+  love.graphics.setBackgroundColor(0, 0, 0, 1)
+
+  for i = 1, 5 do
+    love.graphics.rectangle("line", cells[i].x, 100, cells[i].w, cells[i].h)
+    love.graphics.print(isPressed, cells[i].x + 10, cells[i].y + 15)
+  end
+end
+
+
 
 function love.keypressed(key)
   if key == "escape" then
     love.event.quit(0)
   end
 
-  if key == "a" then
-    cellState = true
+  if key == "space" then
+    cells[currentCell].state = true
+    currentCell = currentCell + 1
+    print(cells[1].state)
+    --print(currentCell)
+  end
 end
