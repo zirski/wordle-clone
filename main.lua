545a06a32e1c73c0e01cd75b0bab0acd42e21e76
zirski <tobiasjhammond@gmail.com>
2022-03-19 19:17:27 -0700

    changed color values so they work

--- main.lua
+++ main.lua
@@ -1,9 +1,11 @@
+g = love.graphics
+
 function love.load()
-  currentCell = 1
+  current = 1
 
-  cells = {}
+  cells = {} -- generates 1 row of cells (for now...)
 
-  for i = 1, 5 do
+  for i = 1, 6 do -- added extra key to avoid program crashing when it thinks it's run out
     cells[i] = {}
 
       cells[i].state = false
@@ -13,32 +15,42 @@ function love.load()
       cells[i].y = 100
       cells[i].w = 50
       cells[i].h = cells[i].w
+
+
+      cells[i].r = 255 / 255
+      cells[i].g = 255 / 255
+      cells[i].b = 255 / 255
+      cells[i].a = 0.7
+
+      cells[i].cellColorState = "line"
   end
 
-  for i, v in ipairs(cells) do
-    print(cells[i].x)
-  end
+  keys = {"a", "b", "c", "d"}
 
 end
 
 function love.update(dt)
-  if cells[currentCell].state then
-    cells[currentCell].isPressed = "true"
-    currentCell = currentCell + 1
+  if cells[current].state == true then -- updates the "if pressed" state of the current cell once "space" is pressed
+
+    cells[current].isPressed = "true"
+    cells[current].a = 1
+
+    current = current + 1
   end
+
+
 end
 
 function love.draw()
 
-  love.graphics.setBackgroundColor(0, 0, 0, 1)
+  g.setBackgroundColor(0, 0, 0, 1)
 
   for i = 1, 5 do
-    love.graphics.rectangle("line", cells[i].x, cells[i].y, cells[i].w, cells[i].h)
-    love.graphics.print(cells[i].isPressed, cells[i].x + 10, cells[i].y + 15)
+    g.setColor(cells[i].r, cells[i].g, cells[i].b, cells[i].a)
+    g.rectangle(cells[i].cellColorState, cells[i].x, cells[i].y, cells[i].w, cells[i].h)
+    g.print(cells[i].isPressed, cells[i].x + 10, cells[i].y + 15)
   end
 end
-
-
 
 function love.keypressed(key)
   if key == "escape" then
@@ -46,8 +58,8 @@ function love.keypressed(key)
   end
 
   if key == "space" then
-    cells[currentCell].state = true
-    print(cells[currentCell].state)
-    --print(currentCell)
+    cells[current].state = true
+    print(cells[current].state)
+    --print(current)
   end
 end
